50a4e3a5014e5370b54a6dbc49be5f3201ed9055
Joaquin Jurado Montalban <jjmontalban@gmail.com>
2023-10-06 00:10:20 +0200

    formulario principal

--- searchform.php
+++ searchform.php
@@ -1,39 +1,33 @@
-<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
-  
-    <label>
-        <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
-        <input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-    </label>
+<div class="search-container">
+    <div class="search-title">
+        <h2>Buscar inmueble</h2>
+    </div>
+    <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+        <!-- Limita la búsqueda a los inmuebles --> 
+        <input type="hidden" name="post_type" value="inmueble" />
 
-    <!-- Limita la busqueda a los inmuebles --> 
-    <input type="hidden" name="post_type" value="inmueble" />
+        <!-- Agrega un select para filtrar por tipo de operación -->
+        <select name="tipo_operacion" id="tipo_operacion">
+            <option value="">Tipo de operación</option>
+            <option value="venta">Venta</option>    
+            <option value="alquiler">Alquiler</option>
+        </select>
 
+        <!-- Agrega un select para filtrar por tipo de inmueble -->
+        <select name="tipo_inmueble" id="tipo_inmueble">
+            <option value="">Todos los inmuebles</option>
+            <?php
+            // Obtén la lista de términos de la taxonomía 'tipo_inmueble'
+            $terms = get_terms('tipo_inmueble');
 
-    <!-- Botones para filtrar por Tipo de Operación -->
-    <label for="tipo_operacion_venta">
-        <input type="radio" name="tipo_operacion" value="venta" id="tipo_operacion_venta">
-        Venta
-    </label>
-    <label for="tipo_operacion_alquiler">
-        <input type="radio" name="tipo_operacion" value="alquiler" id="tipo_operacion_alquiler">
-        Alquiler
-    </label>
-
-    <!-- Agrega un select para filtrar por tipo de inmueble -->
-    <select name="tipo_inmueble" id="tipo_inmueble">
-        <option value="">Todos los inmuebles</option>
-        <?php
-        // Obtén la lista de términos de la taxonomía 'tipo_inmueble'
-        $terms = get_terms('tipo_inmueble');
-
-        if (!empty($terms) && !is_wp_error($terms)) {
-            foreach ($terms as $term) {
-                echo '<option value="' . esc_attr($term->slug) . '">' . esc_html($term->name) . '</option>';
+            if (!empty($terms) && !is_wp_error($terms)) {
+                foreach ($terms as $term) {
+                    echo '<option value="' . esc_attr($term->slug) . '">' . esc_html($term->name) . '</option>';
+                }
             }
-        }
-        ?>
-    </select>
+            ?>
+        </select>
 
-    <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
-
-</form>
+        <input type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+    </form>
+</div>
