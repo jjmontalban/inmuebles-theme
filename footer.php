97089a1e4fe071187db56324615d26d29da1fe3f
JJMontalban <jjmontalban@gmail.com>
2023-12-21 13:44:57 +0100

    Update footer.php and style.css

--- footer.php
+++ footer.php
@@ -1,13 +1,15 @@
 </div>
 
 <footer class="footer" id="page-footer" role="contentinfo">
-	<?php get_sidebar() ?>
-	<div class="site-info row">
-		<div class="columns small-12">
-			<?php _e( 'Chipicasa SL', 'inmuebles' ); ?>
-		</div>
-	</div>
+    <?php get_sidebar('footer'); ?>
+    <div class="site-info row">
+        <div class="columns small-12">
+            <p><?php echo esc_html(get_bloginfo('name')); ?></p>
+            <p><?php echo esc_html(get_bloginfo('description')); ?></p>
+        </div>
+    </div>
 </footer>
+
 
 <?php wp_footer(  ); ?>
 </body>
