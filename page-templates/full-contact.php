e0224fb49c38d4273261fe67408d6a0f8a189aa7
JJMontalban <jjmontalban@gmail.com>
2023-12-21 17:36:18 +0100

    Refactor full-contact.php template

--- page-templates/full-contact.php
+++ page-templates/full-contact.php
@@ -5,26 +5,16 @@
 ?>
 <?php get_header(); ?>
 <main id="main" class="columns large-12 small-12">
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header>
-        <?php if ( has_post_thumbnail() ): ?>
-            <div class="featured-image">
-                <?php the_post_thumbnail( 'full' ); ?>
-            </div>
-        <?php endif; ?>
-        <div class="title-container">
-            <h1 class="post-title"><?php the_title(); ?></h1>
-            <p class="slogan"><?php echo get_post_meta(get_the_ID(), '_eslogan', true); ?></p>  
-            <div class="form-shortcode-container"><?php echo do_shortcode('[formulario_contacto]'); ?></div>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header>
+            </header>
+            <div class="entry-content">
+            <?php the_content(); ?>
+            <?php wp_link_pages(); ?>
         </div>
-    </header>
-    <div class="entry-content">
-        <?php the_content(); ?>
-        <?php wp_link_pages(); ?>
-    </div>
-    <footer>
-        <span class="edit-link"><?php edit_post_link( __( '(Edit)', 'inmuebles' ) ); ?></span>
-    </footer>
+        <footer>
+            <span class="edit-link"><?php edit_post_link( __( '(Edit)', 'inmuebles' ) ); ?></span>
+        </footer>
 </article>
 
 <style>
