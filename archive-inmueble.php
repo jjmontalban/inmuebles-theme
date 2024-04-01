fba390c766e6b070497b07cea8a70c263f02ce93
JJMontalban <jjmontalban@gmail.com>
2024-04-01 16:26:04 +0200

    "Refactor: Adjust font size and link in footer; Move link outside of echo in footer; Update archive-inmueble.php to include card-footer and move permalink to parent div"

--- archive-inmueble.php
+++ archive-inmueble.php
@@ -24,64 +24,73 @@ global $tipos_inmueble_map;
                             $campos = obtener_campos_inmueble($post->ID);  
                             ?> 
                             <div class="col-md-4">
-                                <div class="card-box-a card-shadow">
-                                    <div class="img-box-a">
-                                        <?php if (has_post_thumbnail()) : ?>
-                                            <?php the_post_thumbnail('medium', ['class' => 'img-a img-fluid']); ?>
-                                        <?php endif; ?>
-                                    </div>
-                                    <div class="card-overlay">
-                                        <div class="card-overlay-a-content">
-                                            <div class="card-header-a">
-                                                <h2 class="card-title-a">
-                                                    <a href="<?php the_permalink(); ?>">
-                                                        <?php echo esc_html__( $tipos_inmueble_map[$tipo_inmueble], 'chipicasa' ); ?>
-                                                        <br /> <?php echo esc_html__( 'in', 'chipicasa' ); ?> <?php echo esc_html__( $campos['nombre_calle'], 'chipicasa' ); ?>
-                                                    </a>
-                                                </h2>
-                                            </div>
-                                            <div class="card-body-a">
-                                                <div class="price-box d-flex">
-                                                    <span class="price-a">
-                                                        <?php if ($campos['precio_venta']) : ?>
-                                                            <?php echo $campos['precio_venta']; ?> €
-                                                        <?php else: ?>
-                                                            <?php printf( esc_html__( '%s €/mes', 'chipicasa' ), $campos['precio_alquiler'] ); ?>
-                                                        <?php endif; ?>
-                                                    </span>
+                                <a href="<?php the_permalink(); ?>">
+                                    <div class="card-box-a card-shadow">
+                                        <div class="img-box-a">
+                                            <?php if (has_post_thumbnail()) : ?>
+                                                <?php the_post_thumbnail('medium', ['class' => 'img-a img-fluid']); ?>
+                                            <?php endif; ?>
+                                        </div>
+                                        <div class="card-overlay">
+                                            <div class="card-overlay-a-content">
+                                                <div class="card-header-a">
+                                                    <h2 class="card-title-a">
+                                                            <?php echo esc_html__( $tipos_inmueble_map[$tipo_inmueble], 'chipicasa' ); ?>
+                                                            <br /> <?php echo esc_html__( 'in', 'chipicasa' ); ?> <?php echo esc_html__( $campos['nombre_calle'], 'chipicasa' ); ?>
+                                                        
+                                                    </h2>
                                                 </div>
-                                                <a href="<?php the_permalink(); ?>" class="link-a">
-                                                    <?php echo esc_html__( 'Click here to view', 'chipicasa' ); ?>
-                                                    <span class="ion-ios-arrow-forward"></span>
-                                                </a>
-                                            </div>
-                                            <div class="card-footer-a">
-                                                <ul class="card-info d-flex justify-content-around">
-                                                    <li>
-                                                        <?php if ($campos['m_construidos']) : ?>
-                                                            <h4 class="card-info-title"><?php echo esc_html__( 'Area', 'chipicasa' ); ?></h4>
-                                                            <span>
-                                                                <span><?php echo $campos['m_construidos']; ?></span>m<sup>2</sup>
-                                                            </span>
-                                                        <?php endif; ?> 
-                                                    </li>
-                                                    <li>
-                                                        <?php if ($campos['num_dormitorios']) : ?>
-                                                            <h4 class="card-info-title"><?php echo esc_html__( 'Beds', 'chipicasa' ); ?></h4>
-                                                            <span><?php echo $campos['num_dormitorios']; ?></span>
-                                                        <?php endif; ?>
-                                                    </li>
-                                                    <li>
-                                                        <?php if ($campos['num_banos']) : ?>
-                                                            <h4 class="card-info-title"><?php echo esc_html__( 'Baths', 'chipicasa' ); ?></h4>
-                                                            <span><?php echo $campos['num_banos']; ?></span>
-                                                        <?php endif; ?>
-                                                    </li>
-                                                </ul>
+                                                <div class="card-body-a">
+                                                    <div class="price-box d-flex">
+                                                        <span class="price-a">
+                                                            <?php if ($campos['precio_venta']) : ?>
+                                                                <?php echo $campos['precio_venta']; ?> €
+                                                            <?php else: ?>
+                                                                <?php printf( esc_html__( '%s €/mes', 'chipicasa' ), $campos['precio_alquiler'] ); ?>
+                                                            <?php endif; ?>
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                                <div class="card-footer-a">
+                                                    <ul class="card-info d-flex justify-content-around">
+                                                        <?php if ( $tipo_inmueble == 'terreno' ) { ?>
+                                                                <li>
+                                                                    <?php if ($campos['superf_terreno'] ?? '') : ?>
+                                                                        <h4 class="card-info-title"><?php echo esc_html__( 'Parcela de', 'chipicasa' ); ?></h4>
+                                                                        <span>
+                                                                            <span><?php echo esc_html($campos['superf_terreno']); ?>m<sup>2</sup></span>
+                                                                        </span>
+                                                                    <?php endif; ?> 
+                                                                </li>
+                                                            </ul>
+                                                        <?php } else { ?>
+                                                            <li>
+                                                                <?php if ($campos['m_construidos']) : ?>
+                                                                    <h4 class="card-info-title"><?php echo esc_html__( 'Area', 'chipicasa' ); ?></h4>
+                                                                    <span>
+                                                                        <span><?php echo $campos['m_construidos']; ?></span>m<sup>2</sup>
+                                                                    </span>
+                                                                <?php endif; ?> 
+                                                            </li>
+                                                            <li>
+                                                                <?php if ($campos['num_dormitorios']) : ?>
+                                                                    <h4 class="card-info-title"><?php echo esc_html__( 'Beds', 'chipicasa' ); ?></h4>
+                                                                    <span><?php echo $campos['num_dormitorios']; ?></span>
+                                                                <?php endif; ?>
+                                                            </li>
+                                                            <li>
+                                                                <?php if ($campos['num_banos']) : ?>
+                                                                    <h4 class="card-info-title"><?php echo esc_html__( 'Baths', 'chipicasa' ); ?></h4>
+                                                                    <span><?php echo $campos['num_banos']; ?></span>
+                                                                <?php endif; ?>
+                                                            </li>
+                                                        <?php } ?>
+                                                    </ul>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                </div>
+                                </a>
                             </div>
                         <?php endwhile; ?>
                         <?php get_template_part( 'template-parts/pagination' ); ?>
